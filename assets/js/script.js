c35cbf46a10161c0c3eb088885bee421a0ae837c
nick quinn <nick.quinn@stadium1.com>
2022-03-31 17:30:13 -0400

    Added some more coments and added first confirm! asked if they want special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
 
-function generatePassword(){
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//funciton that prompts user choices and receives answers
+function userChoices(){
 //receive input on password length, check against low of 8 and high of 128
 
 
 //yes or no lowercase, uppercase, mumeric, and/or special characters OR
-//buttons for each type of special character OR // after yes have a text box 
+//buttons for each type of special character OR // after yes have a text box
+var userSpecial = confirm("Do you want special characters");
+
+
+
+}
+
+//array to select each character in the password
+function passwordRandArray(){
+
+
+}
+
+
+function generatePassword(){
+
 
 
 }
