c774f008c3f062d4d81cdac944ef88946e748020
nick quinn <nick.quinn@stadium1.com>
2022-03-31 16:33:23 -0400

    Cleaned up file structure and paths in HTML.  Commented a plan for code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+//receive input on password length, check against low of 8 and high of 128
+
+
+//yes or no lowercase, uppercase, mumeric, and/or special characters OR
+//buttons for each type of special character OR // after yes have a text box 
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
